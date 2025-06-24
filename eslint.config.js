b39e63b1752b9dbc7bb0e5a4468da4e5162d2573
projectmiluju <dnjsdyd0712@naver.com>
2025-06-25 00:03:13 +0900

    chore(setup): eslint.config 생성

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,91 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import path from 'path'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettier from 'eslint-plugin-prettier'
+import importPlugin from 'eslint-plugin-import'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import airbnb from 'eslint-config-airbnb'
+import airbnbHooks from 'eslint-config-airbnb/hooks'
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js'
+import jsxA11yRecommended from 'eslint-plugin-jsx-a11y/configs/recommended.js'
+import prettierRecommended from 'eslint-plugin-prettier/recommended.js'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      prettier,
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        alias: {
+          map: [['@', path.resolve('./src')]],
+          extensions: ['.js', '.jsx'],
+        },
+      },
+    },
+    extends: [
+      js.configs.recommended,
+      airbnb,
+      airbnbHooks,
+      reactRecommended,
+      jsxA11yRecommended,
+      prettierRecommended,
+      reactRefresh.configs.vite,
+    ],
     rules: {
+      // 기본 규칙
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+      'import/prefer-default-export': 'off',
+      'prettier/prettier': 'error',
+
+      // 정렬 규칙
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always',
+        },
+      ],
     },
   },
 ])
