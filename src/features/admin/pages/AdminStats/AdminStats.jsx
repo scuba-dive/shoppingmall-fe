28878784bd58d1e3dd59430d0f58d8277338467d
ssoogit <ssoogit02@gmail.com>
2025-06-29 01:40:08 +0900

    feat(admin-status): 관리자 통계 페이지 html 틀만 잡기

--- src/features/admin/pages/AdminStats/AdminStats.jsx
+++ src/features/admin/pages/AdminStats/AdminStats.jsx
@@ -1,9 +1,31 @@
 import AdminLayout from '@/layouts/AdminLayouts/AdminLayout';
 
+import styles from './AdminStats.module.css';
+
 function AdminStats() {
   return (
     <AdminLayout>
       <h1> 통계 </h1>
+      <div className={styles.container}>
+        <section className={styles.section}>
+          <div>
+            <h2>오늘의 매출 (1시간 별 기준)</h2>
+            <p>12,345,678</p>
+          </div>
+          <div>
+            <h2>오늘의 주문 건 (1시간 별 기준)</h2>
+            <p>15건</p>
+          </div>
+        </section>
+        <section className={styles.section2}>
+          <h2>3일 간 매출 & 주문 수</h2>
+          <div className={styles.box}>그래프</div>
+        </section>
+        <section className={styles.section2}>
+          <h2>오늘의 상품 판매 순위 (1시간 별 기준)</h2>
+          <div className={styles.box}>순위</div>
+        </section>
+      </div>
     </AdminLayout>
   );
 }
