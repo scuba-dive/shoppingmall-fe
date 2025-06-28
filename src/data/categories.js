d06173584978b33bb476096cfd9282345096263e
projectmiluju <dnjsdyd0712@naver.com>
2025-06-29 08:52:22 +0900

    refactor(categories): 이미지 경로를 S3로 변경 및 불필요한 파일 삭제

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,15 +1,28 @@
-import bed from '@/assets/category-bed.png';
-import cabinet from '@/assets/category-cabinet.png';
-import chair from '@/assets/category-chair.png';
-import deco from '@/assets/category-deco.png';
-import table from '@/assets/category-table.png';
-
 const categories = [
-  { name: '침대', image: bed },
-  { name: '테이블', image: table },
-  { name: '의자', image: chair },
-  { name: '수납장', image: cabinet },
-  { name: '소품', image: deco },
+  {
+    name: '침대',
+    image: 'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/category/category-bed.png',
+  },
+  {
+    name: '테이블',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/category/category-table.png',
+  },
+  {
+    name: '의자',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/category/category-chair.png',
+  },
+  {
+    name: '수납장',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/category/category-cabinet.png',
+  },
+  {
+    name: '소품',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/category/category-deco.png',
+  },
 ];
 
 export default categories;
