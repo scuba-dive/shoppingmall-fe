4fb4a0cbb44e032e9ddb6962a4f7039039392454
projectmiluju <dnjsdyd0712@naver.com>
2025-06-29 06:43:18 +0900

    feat(App): 메인 라우터를 MainPage로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import MainRouter from '@/routes/MainRouter';
+import MainPage from '@/pages/MainPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/*" element={<MainRouter />} />
+        <Route path="/*" element={<MainPage />} />
       </Routes>
     </Router>
   );
