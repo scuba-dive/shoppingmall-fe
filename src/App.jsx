803258ce97de6887c3aff058c1001b11577012d7
eunlee <eklee2057@naver.com>
2025-06-29 22:56:18 +0900

    feat(routes): 로그인 및 회원가입 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import MainPage from '@/pages/MainPage';
+import SignIn from '@/pages/SignIn';
+import SignUp from '@/pages/SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+
         <Route path="/*" element={<MainPage />} />
       </Routes>
     </Router>
