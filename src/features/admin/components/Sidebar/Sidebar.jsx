9300349a1f4a352e5dc73ddc16d16d81b4116432
ssoogit <ssoogit02@gmail.com>
2025-06-30 02:58:19 +0900

    refactor(admin-sidebar): 사이드바를 map 메소드를 이용하여 코드 리펙토리 진행

--- src/features/admin/components/Sidebar/Sidebar.jsx
+++ src/features/admin/components/Sidebar/Sidebar.jsx
@@ -2,31 +2,25 @@ import { Link } from 'react-router-dom';
 
 import styles from './Sidebar.module.css';
 
+const menuItem = [
+  { to: '/admin', label: '상품 관리' },
+  { to: '/admin/orders', label: '주문 관리' },
+  { to: '/admin/users', label: '사용자 관리' },
+  { to: '/admin/stats', label: '통계' },
+];
+
 function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <nav>
         <ul>
-          <li>
-            <Link to="/admin" className={styles.menuItem}>
-              상품 관리
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/orders" className={styles.menuItem}>
-              주문 관리
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/users" className={styles.menuItem}>
-              사용자 관리
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/stats" className={styles.menuItem}>
-              통계
-            </Link>
-          </li>
+          {menuItem.map((item) => (
+            <li key={item.to}>
+              <Link to={item.to} className={styles.menuItem}>
+                {item.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </aside>
