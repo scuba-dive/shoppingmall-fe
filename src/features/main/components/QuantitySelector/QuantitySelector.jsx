e0eb54d3e3213a4f9f9d3a440676c4236efdfa04
projectmiluju <dnjsdyd0712@naver.com>
2025-07-02 03:13:51 +0900

    feat(QuantitySelector): 수량 선택기 컴포넌트 및 스타일 추가

---
+++ src/features/main/components/QuantitySelector/QuantitySelector.jsx
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+
+import styles from './QuantitySelector.module.css';
+
+function QuantitySelector({
+  value, //
+  onIncrease, //
+  onDecrease, //
+  onChange, //
+}) {
+  const [inputValue, setInputValue] = useState(value);
+
+  useEffect(() => {
+    setInputValue(value);
+  }, [value]);
+
+  const handleInputChange = (e) => {
+    const newValue = e.target.value;
+    if (/^\d*$/.test(newValue)) {
+      setInputValue(newValue);
+    }
+  };
+
+  const handleBlur = () => {
+    const numericValue = parseInt(inputValue, 10);
+    if (Number.isNaN(numericValue) || numericValue < 1) {
+      setInputValue(1);
+      onChange(1);
+    } else {
+      setInputValue(numericValue);
+      onChange(numericValue);
+    }
+  };
+
+  return (
+    <div className={styles.quantity}>
+      <button type="button" onClick={onDecrease} disabled={value <= 1}>
+        -
+      </button>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        onBlur={handleBlur}
+        inputMode="numeric"
+        pattern="\d*"
+      />
+      <button type="button" onClick={onIncrease}>
+        +
+      </button>
+    </div>
+  );
+}
+
+QuantitySelector.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrease: PropTypes.func.isRequired,
+  onDecrease: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default QuantitySelector;
