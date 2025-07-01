f809d3bbc2c89fcb165b964f4d2ab2f4a69bbaae
projectmiluju <dnjsdyd0712@naver.com>
2025-07-02 02:43:38 +0900

    feat(banner): 배너 이미지 포맷을 PNG에서 WEBP로 변경

--- src/data/banner.js
+++ src/data/banner.js
@@ -1,37 +1,37 @@
 const banner = [
   {
     name: 'main',
-    image: 'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/MainBanner.png',
+    image: 'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/MainBanner.webp',
   },
   {
     name: '카테고리',
     image:
-      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CatergoryBanner.png',
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CatergoryBanner.webp',
   },
   {
     name: '침대',
     image:
-      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Bed.png',
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Bed.webp',
   },
   {
     name: '테이블',
     image:
-      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Table.png',
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Table.webp',
   },
   {
     name: '의자',
     image:
-      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Chair.png',
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Chair.webp',
   },
   {
     name: '수납장',
     image:
-      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Storage.png',
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Storage.webp',
   },
   {
     name: '소품',
     image:
-      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Deco.png',
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Deco.webp',
   },
 ];
 
