751836d2d23771c3943879809e005332b1938f67
projectmiluju <dnjsdyd0712@naver.com>
2025-07-02 01:13:56 +0900

    feat(MainRouter): 상품 상세 페이지 라우트 추가

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import CategoryPage from '@/features/main/pages/CategoriesPage/CategoriesPage';
 import HomePage from '@/features/main/pages/HomePage/HomePage';
+import ProductDetailPage from '@/features/main/pages/ProductDetailPage/ProductDetailPage';
 import ProductListPage from '@/features/main/pages/ProductListPage/ProductListPage';
 import UserLayout from '@/layouts/UserLayouts/UserLayout';
 
@@ -12,6 +13,7 @@ function MainRouter() {
         <Route path="/" element={<HomePage />} />
         <Route path="/category" element={<CategoryPage />} />
         <Route path="/category/:category" element={<ProductListPage />} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
       </Route>
     </Routes>
   );
