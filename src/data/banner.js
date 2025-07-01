6ee71c94437c45ae5b76dc720dd29f5a73b6e534
projectmiluju <dnjsdyd0712@naver.com>
2025-07-01 13:34:37 +0900

    feat(banner): 메인 및 카테고리 배너 데이터 추가

---
+++ src/data/banner.js
@@ -0,0 +1,38 @@
+const banner = [
+  {
+    name: 'main',
+    image: 'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/MainBanner.png',
+  },
+  {
+    name: 'category',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CatergoryBanner.png',
+  },
+  {
+    name: '침대',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Bed.png',
+  },
+  {
+    name: '테이블',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Table.png',
+  },
+  {
+    name: '의자',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Chair.png',
+  },
+  {
+    name: '수납장',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Storage.png',
+  },
+  {
+    name: '소품',
+    image:
+      'https://my-shop-image-bucket.s3.ap-northeast-2.amazonaws.com/Banner/CategoryBanner-Deco.png',
+  },
+];
+
+export default banner;
