02779bb533a96129b78f1dc0b55be8eb081d989e
projectmiluju <dnjsdyd0712@naver.com>
2025-07-03 12:49:34 +0900

    feat(product-list): 상품 목록 로딩 로직 추가 및 카테고리 기반 필터링 구현

--- src/features/main/pages/ProductListPage/ProductListPage.jsx
+++ src/features/main/pages/ProductListPage/ProductListPage.jsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import products from '@/data/products';
 import Breadcrumb from '@/features/main/components/Breadcrumb/Breadcrumb';
 import CategoryNavBar from '@/features/main/components/CategoryNavBar/CategoryNavBar';
 import ProductGrid from '@/features/main/components/ProductGrid/ProductGrid';
+import useCategories from '@/hooks/useCategories';
+import { fetchProductsByCategory } from '@/services/mainService';
 
 function ProductListPage() {
   const { category } = useParams();
+  const { categories, loading: categoryLoading } = useCategories();
 
-  const filteredProducts = products.filter((p) => p.category === category);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const load = async () => {
+      setLoading(true);
+      try {
+        const matched = categories.find((cat) => cat.name === category);
+        if (!matched) throw new Error('카테고리를 찾을 수 없습니다.');
+
+        const data = await fetchProductsByCategory(matched.id, 20);
+        setProducts(data);
+      } catch (err) {
+        //        console.error('상품 목록을 불러오는 데 실패했습니다:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (!categoryLoading && categories.length > 0) {
+      load();
+    }
+  }, [category, categories, categoryLoading]);
 
   return (
     <div>
@@ -23,11 +48,9 @@ function ProductListPage() {
       <ProductGrid
         title={`${category}의 모든 것`}
         showSort
-        onSortChange={(_) => {
-          // console.log('정렬 기준:', _.target.value);
-          // 추후 상태로 정렬 기준 관리 가능
-        }}
-        products={filteredProducts}
+        onSortChange={() => {}}
+        products={products}
+        loading={loading}
       />
     </div>
   );
