cf2ae4fd40d7568e9aa64547b1192ae276b42d21
eunlee <eklee2057@naver.com>
2025-07-07 17:38:40 +0900

    fix(AdminProductSoldoutModal): 모달 스타일 파일 이름 수정

--- src/modals/AdminProductSoldoutModal/AdminProductSoldoutModal.jsx
+++ src/modals/AdminProductSoldoutModal/AdminProductSoldoutModal.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 
 import { updateProductState } from '@/services/adminProductApi';
 
-import styles from './AdminProductSoldOutModal.module.css';
+import styles from './AdminProductSoldoutModal.module.css';
 // 린트가 너무 울어대서 대문자로만 바꿨는데 변경사항 없다고 저장이 안되어 추가하는 주석 멘트
 function AdminProductQuantityModal({
   isOpen, //
